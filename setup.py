dd41bfd6673d233cae3c3b8cb2eb13b3bd971d00
Noah Stebbins <nstebbins@appnexus.com>
2018-12-12 15:03:30 -0500

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='neuralkernel',
-    version='0.0.4',
+    version='0.0.5',
     description='neural networks as a general-purpose computational framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
